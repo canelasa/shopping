41889607d55dbb5d6f659eba02ed44b3adc4cf1b
Anna Canelas <canelasa2@gmail.com>
2017-02-21 19:40:13 +0000

    Create react lists

---
+++ app/assets/javascripts/components/items.jsx
@@ -0,0 +1,18 @@
+var Items = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.items.map(function(item) {
+          return (
+            <div>
+              <h3>{item.name}</h3>
+              <h3>{item.sku}</h3>
+              <h3>{item.price}</h3>
+              <h3>{item.quantity}</h3>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+});
--- app/assets/javascripts/components/lists.jsx
+++ app/assets/javascripts/components/lists.jsx
@@ -1,7 +1,19 @@
 var Lists = React.createClass({
   render: function() {
     return (
-      <h1>Shopping Lists</h1>
+      <div>
+      <h1>My shopping lists</h1>
+        {this.props.lists.map(function(list) {
+          return (
+            <div>
+              <h3>{list.name}</h3>
+              <i>{list.description}</i>
+              <Items items = {items} />
+            </div>
+
+          )
+        })}
+      </div>
     )
   }
 });
