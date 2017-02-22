d5d92b3a2bbfdfb8cfb7a0893730440e92b54c9c
Anna Canelas <canelasa2@gmail.com>
2017-02-22 18:53:10 +0000

    start listform.jsx

---
+++ app/assets/javascripts/components/list_form.jsx
@@ -0,0 +1,14 @@
+var ListForm = React.createClass([
+  render: function() {
+    return (
+      <div>
+        <h2>Create a new shopping list</h2>
+        <form>
+          <input name='name' placeholder='Shopping list name' value={this.props.input_name}/>
+          <input name='description' placeholder='grocery list' value={this.props.input_description}/>
+          <input type='submit' value='Create' />
+        </form>
+      </div>
+    )
+  }
+]);
--- app/assets/javascripts/components/lists.jsx
+++ app/assets/javascripts/components/lists.jsx
@@ -1,14 +1,26 @@
 var Lists = React.createClass({
+  getInitialState: function() {
+    return {
+      appointments: this.props.lists,
+      input_name: '',
+      input_description: ''
+    }
+  },
+
   render: function() {
     return (
       <div>
+
+      <ListForm input_name={this.state.input_name}
+      input_description={this.state.input_description}/>
       <h1>My shopping lists</h1>
-        {this.props.lists.map(function(list) {
+        {this.state.lists.map(function(list) {
           return (
             <div>
               <h3>{list.name}</h3>
               <i>{list.description}</i>
-              <Items items = {list.items} />
+
+              <Items items={this.props.items} />
             </div>
 
           )
